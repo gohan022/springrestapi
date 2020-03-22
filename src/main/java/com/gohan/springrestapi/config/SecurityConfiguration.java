7c918b65a4738a8940b84df1aaf52d5cbbf6606c
gohan022 <soumen.022@gmail.com>
2020-03-22 21:37:36 +0530

    add basic authentication

--- src/main/java/com/gohan/springrestapi/config/SecurityConfiguration.java
+++ src/main/java/com/gohan/springrestapi/config/SecurityConfiguration.java
@@ -1,18 +1,36 @@
 package com.gohan.springrestapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("user").password(passwordEncoder.encode("password")).roles("USER");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .permitAll().and().csrf().disable();
+        http
+                .csrf().disable()
+            .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                //.formLogin().and()
+                .httpBasic();
     }
 }
